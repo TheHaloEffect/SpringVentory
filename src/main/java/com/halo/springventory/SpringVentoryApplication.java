20cce4ae703f0580d12a82e75133e744f0d02b13
TheHaloEffect <githubemail165@gmail.com>
2023-10-29 14:24:10 +0000

    project initalisation

---
+++ src/main/java/com/halo/springventory/SpringVentoryApplication.java
@@ -0,0 +1,13 @@
+package com.halo.springventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVentoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVentoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/halo/springventory/SpringVentoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halo.springventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVentoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
